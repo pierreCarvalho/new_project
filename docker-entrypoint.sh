f7ac6ab2fb264f7af979e43625706e71ab0d5c0c
pierre fenner <pierre.fenner@gestaods.com.br>
2024-10-01 22:42:02 -0300

    initial commit

---
+++ docker-entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Apply database migrations
+echo "Apply database migrations"
+python3 manage.py makemigrations
+python3 manage.py migrate
+# Start server
+echo "Starting server"
+python3 manage.py runserver 0.0.0.0:8000
